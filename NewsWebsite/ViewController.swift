6a79f8e5481bb0b3ce44cc25ce38a5c66ae9f63c
Artem Manakov <artem.manakov29@gmail.com>
2023-01-26 10:45:18 +0200

    Made Post (“Post” screen that shows a particular post (in a WebView too). It should be opened once a user taps on a post in the “Feed” screen.)

--- NewsWebsite/ViewController.swift
+++ NewsWebsite/ViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKNavigationDelegate {
-    var webView: WKWebView!
-    var progressView: UIProgressView!
-    
+class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
+    private var webView: WKWebView!
+    private var progressView: UIProgressView!
+    var url: URL = URL(string: "https://theatlasnews.co/ml-api/v2/list")!
+
     override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
@@ -20,25 +21,45 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureWebView()
+        setupWebView()
+    }
+    
+    func configureWebView() {
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        setToolbar()
+        webView.addObserver(self,
+                            forKeyPath: #keyPath(WKWebView.estimatedProgress),
+                            options: .new,
+                            context: nil)
+    }
+    
+    func setToolbar() {
         let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
-        let forwardButton = UIBarButtonItem(image: UIImage(systemName: "chevron.forward"), style: .plain, target: webView, action: #selector(webView.goForward))
-        let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: webView, action: #selector(webView.goBack))
         
         progressView = UIProgressView(progressViewStyle: .default)
         progressView.sizeToFit()
         let progressButtom = UIBarButtonItem(customView: progressView)
-
-        toolbarItems = [backButton, space, forwardButton, space, progressButtom, space, refresh]
+        
+        toolbarItems = [space, progressButtom, space, refresh, space]
         navigationController?.isToolbarHidden = false
-        
-        
-        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
-        
-        let url = URL(string: "https://theatlasnews.co/ml-api/v2/list")!
-        webView.load(URLRequest(url: url))
-        webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    func setupWebView() {
+        guard let path = Bundle.main.path(forResource: "jsCode", ofType: "js") else {
+            print("Incorrect filepath")
+            return
+        }
+        guard let jsCode = try? String(contentsOfFile: path) else {
+            print("Something went wrong with converting path into string")
+            return
+        }
+                
+        let userScript = WKUserScript.init(source: jsCode, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        webView.configuration.userContentController.addUserScript(userScript)
+        webView.configuration.userContentController.add(self, name: "messenger")
     }
     
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
@@ -49,15 +70,27 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         let url = navigationAction.request.url
-        
         if let host = url?.host {
-            if host.contains("theatlasnews") {
+            if host.contains("theatlasnews.co") {
                 decisionHandler(.allow)
                 return
             }
         }
         decisionHandler(.cancel)
-
+    }
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        print(message.body)
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Feed") as? ViewController {
+            guard let url = URL(string: "https://theatlasnews.co/ml-api/v2/post?post_id=\(message.body)") else {
+                print("Incorrect URL")
+                return
+            }
+            vc.url = url
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 }
+
+
 
